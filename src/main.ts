51d84aa35978296a33cd26edd26206006e733f60
Pawel Kreski <Pawel.Kreski@hyland.com>
2023-01-24 13:59:34 +0100

    test

--- src/main.ts
+++ src/main.ts
@@ -198,6 +198,10 @@ class TestReporter {
     core.info(`Check run create response: ${resp.status}`)
     core.info(`Check run URL: ${resp.data.url}`)
     core.info(`Check run HTML: ${resp.data.html_url}`)
+
+    const { exec } = require("child_process");
+
+    exec(`export HTML_REPORT_URL=${resp.data.html_url}`, () => {});
     process.env.HTML_REPORT_URL = resp.data.html_url ? resp.data.html_url : 'lol.pl';
     process.env['HTML_REPORT_URL'] = resp.data.html_url ? resp.data.html_url : 'lol.pl';
 
